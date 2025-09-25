e6c56fcfb3b124142ebed93003f6bf16590d4e18
seiryuu0208-art <seiryuu0208@gmail.com>
2025-09-25 14:54:12 +0900

    Add files via upload

---
+++ ガルヒ鍼灸院/js/scroll.js
@@ -0,0 +1,14 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+    $('a[href^=#]').click(function(){
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
+</script>
